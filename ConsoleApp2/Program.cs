567d5e4ec59643019bcc2b6571eb59b425bd2d71
Klinchuk <vladik.klin@gmail.com>
2023-10-20 15:42:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static int[] Input()
+    {
+        Console.Write(" n = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write("a[{ 0}]= ", i);
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        return a;
+    }
+    static int F(int[] a)
+    {
+        int k = 0;
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (a[i] == a[i + 1])
+            {
+                ++k;
+            }
+        }
+        return k;
+    }
+    static void Main()
+    {
+        int[] a = Input();
+        Console.WriteLine("k ={ 0} ", F(a));
+    }
+}
